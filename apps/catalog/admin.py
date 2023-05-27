1e497ea9e9718df1a40ab59285d16ea724a5d0e8
satel31 <blokhnina.tatiana@yandex.ru>
2023-05-27 18:22:52 +0300

    Customized the display of the models in the admin panel

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from apps.catalog.models import Category, Product
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'category_name')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'product_name', 'unit_price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('product_name', 'description',)
+
+
