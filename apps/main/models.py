23f02af14401b3b815c5848ae805dda637a8f7f5
satel31 <blokhnina.tatiana@yandex.ru>
2023-05-27 17:18:40 +0300

    Created drafts of models

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    pass
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+class Category(models.Model):
+    pass
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
